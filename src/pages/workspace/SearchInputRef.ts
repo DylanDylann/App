09f05fa982265538dfb4a80954613d88f2f01a3d
DylanDylann <dylandylan9112@gmail.com>
2023-11-10 17:27:35 +0700

    fix: redundant code

--- src/pages/workspace/SearchInputRef.ts
+++ src/pages/workspace/SearchInputRef.ts
@@ -1,11 +1,4 @@
-import React from 'react';
-import {TextInput} from 'react-native';
-
-const searchInputRef = React.createRef<TextInput>();
-let onSearchInputChange: (text: string) => void = () => {};
 let searchInput: string = '';
 export default {
-    searchInputRef,
-    onSearchInputChange,
     searchInput,
 };
